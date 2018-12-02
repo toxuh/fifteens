30d747c8d46532944950d4721d4dba5b6a6ea438
toxuh <zakharov.toxa@gmail.com>
2018-12-02 16:10:51 +0400

    basic logic. move empty cell, check if win

--- src/App.js
+++ src/App.js
@@ -8,30 +8,36 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; //16th for null
+        this.array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; //16th for empty cell
 
         this.state = {
             mixed: null
         };
     }
 
-    componentDidMount() {
+    componentDidMount = () => {
         this.mixCells()
-    }
+    };
 
     mixCells = () => {
         // Get numbers array, mix it, concat empty cell
-        const mixedArray = this.array.sort(() => Math.random()-.5).concat(0);
+        const mixedArray = this.array.sort(() => Math.random()-.5).concat('empty');
 
         this.setState({
             mixed: mixedArray
         })
     };
 
+    updateMixedCells = (arr) => {
+        this.setState({
+            mixed: arr
+        });
+    };
+
     render() {
         return (
             <div className="App">
-                {this.state.mixed && <Game array={this.state.mixed} />}
+                {this.state.mixed && <Game array={this.state.mixed} onUpdate={this.updateMixedCells} />}
             </div>
         );
     }
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -5,6 +5,64 @@ import './Game.css';
 class Game extends Component {
     constructor(props) {
         super(props);
+
+        this.steps = [-4, 1, +4, -1]; // Top, right, down, left
+
+        this.state = {
+            win: false,
+            emptyIndex: 15 // Last cell on board
+        };
+    }
+
+    clickHandler = (e) => {
+        const { emptyIndex } = this.state;
+        const id = e.target.innerText;
+
+        if (id) { // If possible, move
+            const index = this.props.array.findIndex(i => i === parseInt(id));
+
+            this.steps.forEach(step => {
+                if (
+                    ((emptyIndex + step < 16) && (emptyIndex + step === index)) ||
+                    ((emptyIndex + step > 0) && (emptyIndex + step === index))
+                ) {
+                    this.move(index);
+                }
+            })
+        }
+    };
+
+    move = (newIndex) => {
+        const { emptyIndex } = this.state;
+        const arr = this.props.array;
+        const empty = arr[emptyIndex];
+        const newValue = arr[newIndex];
+
+        arr[newIndex] = empty;
+        arr[emptyIndex] = newValue;
+
+        this.setState({
+            emptyIndex: newIndex
+        });
+
+        this.props.onUpdate(arr);
+
+        this.checkResult();
+    };
+
+    checkResult() {
+        const { array } = this.props;
+        const notValidItems = [];
+
+        for (let i = 0; i < 14; i++) {
+            const test = (array[i] + 1 !== array[i + 1]) && notValidItems.push(array[i + 1]);
+        }
+
+        if (notValidItems.length === 0) {
+            this.setState({
+                win: true
+            })
+        }
     }
 
     render() {
@@ -13,12 +71,23 @@ class Game extends Component {
         return (
             <div className="Game">
                 {array.map(number => {
-                    const empty = number === 0 ? 'empty' : '';
+                    const empty = number === 'empty' ? 'empty' : '';
 
                     return (
-                        <div className={`Game__cell ${empty}`}>{ number !== 0 && number }</div>
+                        <div
+                            key={ number }
+                            className={`Game__cell ${empty}`}
+                            onClick={this.clickHandler}
+                        >
+                            { number !== 'empty' && number }
+                        </div>
                     )
                 })}
+                <h1>
+                    {this.state.win && (
+                        <span>WIN!!!</span>
+                    )}
+                </h1>
             </div>
         )
     }
